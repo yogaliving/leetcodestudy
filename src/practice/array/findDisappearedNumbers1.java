5724c5bfb2d0a0a1f72675f3b91d993a9061ac02
quyueliang <846947743@qq.com>
2021-03-23 20:07:33 +0800

    0323

---
+++ src/practice/array/findDisappearedNumbers1.java
@@ -0,0 +1,19 @@
+package practice.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class findDisappearedNumbers1 {
+    List<Integer> list = new LinkedList<>();
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[nums[i]] = -Math.abs(nums[nums[i]]);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i]>0) list.add(nums[i]);
+        }
+        return list;
+    }
+}
